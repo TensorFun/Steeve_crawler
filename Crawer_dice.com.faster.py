099cf8c338a2af9fdda05c6fe7d65ad398bdd170
Cheerlemon <q7519285@gmail.com>
2018-03-24 23:50:04 +0800

    fix bug and testing 90s

--- Crawer_dice.com.faster.py
+++ Crawer_dice.com.faster.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[198]:
+# In[1]:
 
 
 import requests
@@ -13,7 +13,7 @@ import time
 domain_name = 'https://www.dice.com'
 
 
-# In[199]:
+# In[2]:
 
 
 ## Input you want
@@ -23,7 +23,7 @@ key_main=domain_name+'/jobs/'+'q-'+Key_work+'-l-'+Key_location+'-radius-30-start
 key_main
 
 
-# In[235]:
+# In[3]:
 
 
 ## Calculate Job Pages
@@ -33,7 +33,7 @@ pages = int(str(soup.find("",{"id":"posiCountId"}).text).replace(',',''))//30
 print('Total have '+str(pages)+' pages')
 
 
-# In[ ]:
+# In[5]:
 
 
 ## Add all pages's work
@@ -54,122 +54,143 @@ for i in range(pages-10):
 print("Total have : ",len(herf))
 
 
-# In[215]:
+# In[6]:
 
 
 def fetch_article_content(url):
-    try:
-        response_work = requests.get(url)
-        soup = BeautifulSoup(response_work.text,'lxml')
-        jobTitle = soup.find("",{"class":"jobTitle"}).text
-        jobEmployer = soup.find("",{"class":"employer"}).text.replace('\n','').replace('\t','')
-        jobLocation = soup.find("",{"class":"location"}).text.replace('\n','')
-        jobPostTime = soup.find("",{"class":"posted hidden-xs"}).text
-        jobID = soup.find("",{"class":"company-header-info"}).text
-        jobID = jobID.replace('\n','').split(':')[2].strip().replace('-','')
-        ## tag job-info is a array .
-        foo = soup.find_all("",{"class":"row job-info"})
-        
-        out = []
-        for o in foo:
-            out.append(o.text.replace('\n','').replace('\t',''))
+    judege  = True
+    
+    response_work = requests.get(url)
+    soup = BeautifulSoup(response_work.text,'lxml')
+    if soup.find("",{"class":"pull-left h1 jobs-page-header-h1"}):
+        judege = False
+    if soup.find("",{"class":"col-md-12 error-page-header"}):
+        judege = False
+    
+    
+    if judege==True:
+        try:
             
-    except AttributeError:
-        jobTitle='None'
-        jobEmployer='None'
-        jobLocation='None'
-        jobPostTime='None'
-        foo='None'
-        jobID='None'
-        #jobDescription=''   
-        
-    try:
-        jobskillss = out[0]
-        jobemploymentType = out[1]
-        jobbaseSalary = out[2]
+            jobTitle = soup.find("",{"class":"jobTitle"}).text
+            
+            jobEmployer = soup.find("",{"class":"employer"}).text.replace('\n','').replace('\t','')
+            jobLocation = soup.find("",{"class":"location"}).text.replace('\n','')
+            jobPostTime = soup.find("",{"class":"posted hidden-xs"}).text
+            jobID = soup.find("",{"class":"company-header-info"}).text
+            jobID = jobID.replace('\n','').split(':')[2].strip().replace('-','')
+            ## tag job-info is a array .
+            foo = soup.find_all("",{"class":"row job-info"})
 
-    except IndexError:
-        jobskillss = 'None'
-        jobemploymentType = 'None'
-        jobbaseSalary = 'None'
+            out = []
+            for o in foo:
+                out.append(o.text.replace('\n','').replace('\t',''))
+
+        except AttributeError:
+            jobTitle='None'
+            jobEmployer='None'
+            jobLocation='None'
+            jobPostTime='None'
+            foo='None'
+            jobID='None'
+            #jobDescription=''   
+
+        try:
+            jobskillss = out[0]
+            jobemploymentType = out[1]
+            jobbaseSalary = out[2]
+
+        except IndexError:
+            jobskillss = 'None'
+            jobemploymentType = 'None'
+            jobbaseSalary = 'None'
+
+    ## Jobdescription
+        str1 = str(soup.find("",{"id":"jobdescSec"}))
+        soup = BeautifulSoup(str1.replace('<br/>','\n').replace('</li>','\n').replace('</strong>','\n').replace('</p>','\n').replace('<p>','\n'),'lxml')
+        jobDescription = soup.text
+        joburl = url
+    else:
+        return
         
-## Jobdescription
-    str1 = str(soup.find("",{"id":"jobdescSec"}))
-    soup = BeautifulSoup(str1.replace('<br/>','\n').replace('</li>','\n').replace('</strong>','\n').replace('</p>','\n').replace('<p>','\n'),'lxml')
-    jobDescription = soup.text
-    joburl = url
     
     return((jobTitle,jobEmployer,jobLocation,jobPostTime,jobID,jobskillss,jobemploymentType,jobbaseSalary,joburl,jobDescription))
 
 
-# In[216]:
+# In[7]:
 
 
 def load_to_json(content_arry):
     flag=True
     for i in content_arry:
-        jobTitle = i[0]
-        jobEmployer = i[1]
-        jobLocation = i[2]
-        jobPostTime = i[3]
-        jobID = i[4]
-        jobskills = i[5]
-        jobemploymentType = i[6]
-        jobbaseSalary = i[7]
-        joburl = i[8]
-        jobDescription = i[9]
+        if i:
+            jobTitle = i[0]
+            jobEmployer = i[1]
+            jobLocation = i[2]
+            jobPostTime = i[3]
+            jobID = i[4]
+            jobskills = i[5]
+            jobemploymentType = i[6]
+            jobbaseSalary = i[7]
+            joburl = i[8]
+            jobDescription = i[9]
         
-        if flag==True:
-            data = {Key_work:[{
-                         "jobID":jobID,
-                         "jobTitle":jobTitle,
-                         "jobEmployer":jobEmployer,
-                         "jobLocation":jobLocation,
-                         "jobPostTime":jobPostTime,
-                         "jobskills":jobskills,
-                         "jobemploymentType":jobemploymentType,
-                         "jobbaseSalary":jobbaseSalary,
-                         "joburl":joburl,
-                         "jobDescription":jobDescription,
-                             }]}
-            flag=False
-            
-            
-        else:
-            add_data = {    "jobID":jobID,  
-                            "jobTitle":jobTitle,
-                            "jobEmployer":jobEmployer,
-                            "jobLocation":jobLocation,
-                            "jobPostTime":jobPostTime,
-                            "jobskills":jobskills,
-                            "jobemploymentType":jobemploymentType,
-                            "jobbaseSalary":jobbaseSalary,
-                            "joburl":joburl,
-                            "jobDescription":jobDescription  }
-            data[Key_work].append(add_data)
+            if flag==True:
+                data = {Key_work:[{
+                             "jobID":jobID,
+                             "jobTitle":jobTitle,
+                             "jobEmployer":jobEmployer,
+                             "jobLocation":jobLocation,
+                             "jobPostTime":jobPostTime,
+                             "jobskills":jobskills,
+                             "jobemploymentType":jobemploymentType,
+                             "jobbaseSalary":jobbaseSalary,
+                             "joburl":joburl,
+                             "jobDescription":jobDescription,
+                                 }]}
+                flag=False
+
+
+            else:
+                add_data = {    "jobID":jobID,  
+                                "jobTitle":jobTitle,
+                                "jobEmployer":jobEmployer,
+                                "jobLocation":jobLocation,
+                                "jobPostTime":jobPostTime,
+                                "jobskills":jobskills,
+                                "jobemploymentType":jobemploymentType,
+                                "jobbaseSalary":jobbaseSalary,
+                                "joburl":joburl,
+                                "jobDescription":jobDescription  }
+                data[Key_work].append(add_data)
     return data
 
 
-# In[217]:
+# In[28]:
 
 
 #testing
+#post_links = herf
 post_links = herf
-#post_links = herf[1:100]
 start = time.time()
-with Pool(processes=20) as pool:
+with Pool(processes=70) as pool:
     contents = pool.map(fetch_article_content, post_links)
 end = time.time()
 print(end-start)
 
 
-# In[218]:
+# In[ ]:
 
 
-result = load_to_json(contents)
+result = load_to_json(con)
 
 
-# In[227]:
+# In[30]:
+
+
+len(result[Key_work])
+
+
+# In[10]:
 
 
 #過濾條件#
@@ -182,7 +203,7 @@ rules = ['frontend','front-end','UI','web design','web designer','front end']
 #rules = ['project management']
 
 
-# In[230]:
+# In[11]:
 
 
 data = {Key_work:[]}
@@ -194,10 +215,22 @@ for i in range(len(result[Key_work])):
             data[Key_work].append(result[Key_work][i])
 
 
-# In[234]:
+# In[12]:
 
 
 with open(Key_work+".json","w") as f:
     json.dump(data,f)
     print('Save to json is ok')
+
+
+# In[13]:
+
+
+get_ipython().system('jupyter nbconvert --to script Crawer_dice.com.faster.ipynb')
+
+
+# In[94]:
+
+
+
 
